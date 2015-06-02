b5e1b8997df509c6d79cc1caf1381eee21a7a620
Jan Dula <frantisheq@gmail.com>
2015-06-02 14:20:30 -0700

    mkvendor.sh: Initial commit of kingdom_row

---
+++ AndroidProducts.mk
@@ -0,0 +1,2 @@
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/device_kingdom_row.mk
---
+++ cm.mk
@@ -0,0 +1,18 @@
+## Specify phone tech before including full_phone
+$(call inherit-product, vendor/cm/config/gsm.mk)
+
+# Release name
+PRODUCT_RELEASE_NAME := kingdom_row
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/lenovo/kingdom_row/device_kingdom_row.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := kingdom_row
+PRODUCT_NAME := cm_kingdom_row
+PRODUCT_BRAND := lenovo
+PRODUCT_MODEL := kingdom_row
+PRODUCT_MANUFACTURER := lenovo
