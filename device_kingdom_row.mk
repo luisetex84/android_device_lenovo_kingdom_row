8cd73840b7b44f12d575b7be5451186fe0c36b51
frantisheq <frantisheq@gmail.com>
2015-06-10 16:12:16 +0200

    Update device_kingdom_row.mk

--- device_kingdom_row.mk
+++ device_kingdom_row.mk
@@ -47,12 +47,12 @@ PRODUCT_PACKAGES += \
     
 PRODUCT_PACKAGES += \
     libc2dcolorconvert \
-    libstagefrighthw \
     libOmxCore \
     libmm-omxcore \
     libOmxVdec \
     libOmxVdecHevc \
     libOmxVenc
+#    libstagefrighthw
 
 PRODUCT_PACKAGES += \
     audio.primary.msm8974 \
