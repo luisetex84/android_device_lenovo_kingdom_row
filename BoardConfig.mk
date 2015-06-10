03a5a83b55387e3f8603e16f8501b1215228ee2d
frantisheq <frantisheq@gmail.com>
2015-06-10 14:58:36 +0200

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,7 +1,7 @@
 USE_CAMERA_STUB := true
 
 # inherit from the proprietary version
--include vendor/lenovo/kingdom_row/BoardConfigVendor.mk
+#-include vendor/lenovo/kingdom_row/BoardConfigVendor.mk
 
 TARGET_ARCH := arm
 TARGET_NO_BOOTLOADER := true
