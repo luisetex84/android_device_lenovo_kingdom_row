5f4008194a2fab37079a9ecc5f2806b58e5a5be5
Jan Dula <frantisheq@gmail.com>
2015-06-18 11:07:27 +0200

    fstab.qcom

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -3,9 +3,9 @@ LOCAL_PATH := $(call my-dir)
 # Init scripts
 
 include $(CLEAR_VARS)
-LOCAL_MODULE            := recovery.fstab
+LOCAL_MODULE            := fstab.qcom
 LOCAL_MODULE_TAGS       := optional eng
 LOCAL_MODULE_CLASS      := ETC
-LOCAL_SRC_FILES         := etc/recovery.fstab
+LOCAL_SRC_FILES         := etc/fstab.qcom
 LOCAL_MODULE_PATH       := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
