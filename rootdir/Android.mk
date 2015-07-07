489a0f71e6d9a8d72b822ba53324883fdfba9af3
Jan Dula <frantisheq@gmail.com>
2015-07-07 12:38:08 +0200

    fix fstab building

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -4,10 +4,10 @@ include $(CLEAR_VARS)
 # Device init files
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := fstab.bacon
+LOCAL_MODULE       := fstab.kingdom_row
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/fstab.bacon
+LOCAL_SRC_FILES    := etc/fstab.kingdom_row
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
