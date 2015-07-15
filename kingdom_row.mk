aec8043fbccd76ae526e628f923b4de33ec2e60a
Jan Dula <frantisheq@gmail.com>
2015-07-15 23:39:52 +0200

    add mixer paths file

--- kingdom_row.mk
+++ kingdom_row.mk
@@ -46,6 +46,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
+    $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths_auxpcm.xml \
     $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml
 
 # Wi-Fi
