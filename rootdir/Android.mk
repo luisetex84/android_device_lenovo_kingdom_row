b3161c162e2ec655ce2281afb4b133bddbfcea90
Jan Dula <frantisheq@gmail.com>
2015-07-20 23:24:50 +0200

    really fix the build

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -352,11 +352,3 @@ LOCAL_SRC_FILES    := etc/init.qcom.ssr.sh
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
-include $(CLEAR_VARS)
-LOCAL_MODULE       := ueventd.qcom.rc
-LOCAL_MODULE_TAGS  := optional eng
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/ueventd.qcom.rc
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
-include $(BUILD_PREBUILT)
-
