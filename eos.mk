6ae39db3d513b51df782d923e6912663c4f3ea24
Jan Dula <frantisheq@gmail.com>
2015-08-23 17:06:57 +0200

    euphorize

--- eos.mk
+++ eos.mk
@@ -18,13 +18,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from kingdom_row device
 $(call inherit-product, device/lenovo/kingdom_row/kingdom_row.mk)
 
-# Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
-
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/eos/config/common.mk)
+$(call inherit-product, vendor/eos/config/common_full_phone.mk)
 
-PRODUCT_NAME := cm_kingdom_row
+PRODUCT_NAME := eos_kingdom_row
 PRODUCT_DEVICE := kingdom_row
 PRODUCT_MANUFACTURER := LENOVO
 PRODUCT_MODEL := Lenovo K920
@@ -37,9 +35,15 @@ TARGET_VENDOR_PRODUCT_NAME := kingdom_row
 TARGET_VENDOR_DEVICE_NAME := kingdom_row
 PRODUCT_BUILD_PROP_OVERRIDES += TARGET_DEVICE=kingdom_row PRODUCT_NAME=kingdom_row
 
+TARGET_CONTINUOUS_SPLASH_ENABLED := true
+
 ## Use the latest approved GMS identifiers unless running a signed build
 ifneq ($(SIGN_BUILD),true)
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BUILD_FINGERPRINT=Lenovo/kingdom_row/kingdom_row:5.0.2/LRX22G/K920_S246_150520_ROW:user/release-keys \
     PRIVATE_BUILD_DESC="kingdom_row/kingdom_row-user 5.0.2 LRX22G/K920_S246_150520_ROW release-keys"
 endif
+
+# Copy device specific prebuilt files.
+PRODUCT_COPY_FILES += \
+    vendor/eos/prebuilt/common/bootanimations/BOOTANIMATION-1080x1920.zip:system/media/bootanimation.zip
