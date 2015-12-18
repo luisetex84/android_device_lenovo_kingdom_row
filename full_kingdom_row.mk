ab22f289f741ae4b30d267856c8c428d90b2d8c0
Jan Dula <frantisheq@gmail.com>
2015-12-18 23:25:55 +0100

    aicpize

--- full_kingdom_row.mk
+++ full_kingdom_row.mk
@@ -16,7 +16,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from kingdom_row device
-$(call inherit-product, device/lenovo/kingdom_row/kingdom_row.mk)
+$(call inherit-product, device/lenovo/kingdom_row/device.mk)
 
 # Enhanced NFC
 $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
